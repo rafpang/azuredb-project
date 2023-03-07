a5cc4bfe608b63011286cd1e5eaae6928f6a6d55
rafpang <rafpang>
2023-03-07 03:26:18 +0000

    updated analytics notebook

--- analytics-notebooks/main-notebook.py
+++ analytics-notebooks/main-notebook.py
@@ -19,7 +19,7 @@ results = spark.read \
 races = spark.read \
         .option("header",True)\
         .option("inferSchema" , True) \
-        .csv("dbfs:/mnt/databricksf1projectdl/raw/races.csv").select('raceId','year','name','round')        
+        .csv("dbfs:/mnt/databricksf1projectdl/raw/races.csv").withColumnRenamed("name","circuit_name").select('raceId','year','circuit_name','round')        
 
 constructors = spark.read \
         .option("header",True)\
@@ -30,7 +30,8 @@ constructors = spark.read \
 #Joining the dataframes
 df = results.join(races, results["raceID"] == races["raceID"], 'left') 
 df= df.join(drivers, df["driverId"] == drivers["driverId"], 'left').withColumnRenamed('nationality','driver_nationality') 
-df = df.join(constructors, df["constructorId"] == constructors["constructorId"], 'left').withColumnRenamed('nationality','constructor_nationality') 
+df = df.join(constructors, df["constructorId"] == constructors["constructorId"],'left').withColumnRenamed('nationality','constructor_nationality') \
+        .withColumnRenamed("name","constructors_name")
 
 
 
@@ -52,6 +53,31 @@ display(df.groupBy("driver_nationality").count().orderBy("count", ascending=Fals
 #Which drivers have (or had) the most grandprix wins?
 
 display(df.filter(df.positionOrder == 1).select('driverRef','resultId').groupBy('driverRef').count().orderBy("count", ascending=False).limit(10)) 
+
+# COMMAND ----------
+
+#Winningest Constructors
+display(df.filter(df.positionOrder == 1).select('constructors_name','resultId').groupBy('constructors_name').count().orderBy("count", ascending=False).limit(10)) 
+
+# COMMAND ----------
+
+#My favorite team is Ferrari, Ferrari hasn't had the best of seasons of late. How have they performed in the 2000s
+display(df.filter(df.constructors_name == "Ferrari").select('year','positionOrder').filter((df.positionOrder==1)& (df.year >=2000)).groupBy('year').count())
+
+
+# COMMAND ----------
+
+#Ferrari's strongest tracks (by points)
+
+display(df.filter((df.constructors_name=="Ferrari") & (df.year >= 2000)).select('circuit_name','points').groupBy('circuit_name').sum('points').orderBy('sum(points)', ascending =False))
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
